b641637ac8578c5f8df12c2a8eadc832f45bfab6
Jessica-Vieira <jessicavieira661@gmail.com>
2025-05-06 01:05:57 -0300

    nextconfig alterada

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = (process.env.NODE_ENV = "production");
 const nextConfig = {
-  basePath: isProd ? "/veterinarioSite" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/veterinarioSite" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/veterinarioSite" : "",
   output: "export",
   reactStrictMode: true,
 };
