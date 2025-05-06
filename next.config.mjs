b99297359f6f9461c70e31fc315192ea76667ddf
Jessica-Vieira <jessicavieira661@gmail.com>
2025-05-06 00:53:30 -0300

    nextconfig alterada

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = (process.env.NODE_ENV = "production");
 const nextConfig = {
-  basePath: "/veterinarioSite",
+  basePath: isProd ? "/veterinarioSite" : "",
   output: "export",
   reactStrictMode: true,
 };
